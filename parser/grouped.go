5b6d7c47e04ac3495a8e53b8e803a66b74de4ba4
Christopher Martinez <indychris239@gmail.com>
2026-01-03 15:14:48 -0500

    Add grouped parser

---
+++ parser/grouped.go
@@ -0,0 +1,18 @@
+package parser
+
+import (
+	"ego/ast"
+	"ego/token"
+)
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
+}
