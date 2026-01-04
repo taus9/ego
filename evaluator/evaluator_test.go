104c5922f06013e702beb0db8b1d169679ef69fd
Christopher Martinez <indychris239@gmail.com>
2026-01-04 17:40:12 -0500

    Add return eval

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -152,3 +152,29 @@ func testNilObject(t *testing.T, obj object.Object) bool {
 	}
 	return true
 }
+
+func TestReturnStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"ret 10", 10},
+		{"ret 10 \n 9", 10},
+		{"ret 2 * 5 \n 9", 10},
+		{"9 \n ret 2 * 5 \n 9", 10},
+		{
+			`if 10 > 1
+				if 10 > 1
+					ret 10
+				;
+				ret 1
+			;`,
+			10,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
