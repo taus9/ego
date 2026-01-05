f30c8872cb20517b7015dfd4f920c9b9ef26a4c3
Christopher Martinez <indychris239@gmail.com>
2026-01-05 12:28:21 -0500

    Add function calls

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -172,6 +172,23 @@ func TestReturnStatements(t *testing.T) {
 			;`,
 			10,
 		},
+		{
+			`f = :> x
+				ret x
+				x + 10
+			;
+			f(10)`,
+			10,
+		},
+		{
+			`f = :> x
+				result = x + 10
+				ret result
+				ret 10
+			;
+			f(10)`,
+			20,
+		},
 	}
 
 	for _, tt := range tests {
@@ -248,6 +265,47 @@ func TestLetStatements(t *testing.T) {
 		{"a = 5 \n b = a \n c = a + b + 5 \n c", 15},
 	}
 
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestAnonFunctionObject(t *testing.T) {
+	input := ":> x \n x + 2 \n ;"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not Function. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong number of parameters. got=%d", len(fn.Parameters))
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"identity = :> x \n x \n ; \n identity(5)", 5},
+		{"identity = :> x \n ret x \n ; \n identity(5)", 5},
+		{"double = :> x \n x * 2 \n ; \n double(5)", 10},
+		{"add = :> x, y \n x + y \n ; \n add(5, 5)", 10},
+		{"add = :> x, y \n x + y \n ; \n add(5 + 5, add(5, 5))", 20},
+		{":> x \n x \n ;(5)", 5},
+	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testIntegerObject(t, evaluated, tt.expected)
