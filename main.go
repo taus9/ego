5deebb9877202d1acfae8e0e99c1435a0846d531
Christopher Martinez <indychris239@gmail.com>
2026-01-06 14:14:21 -0500

    Refactor CLI

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"ego/internal/repl"
+	"ego/internal/backend/eval/evaluator"
+	"ego/internal/backend/eval/object"
+	"ego/internal/frontend/lexer"
+	"ego/internal/frontend/parser"
 	"fmt"
+	"io"
 	"os"
-	"os/user"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	userArgs := os.Args[1:]
+	if len(userArgs) == 0 {
+		fmt.Println("No file provided.")
+		os.Exit(1)
 	}
-	fmt.Printf(
-		"Hello %s! This is the Monkey programming language!\n",
-		user.Username,
-	)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+
+	filename := userArgs[0]
+
+	source, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	l := lexer.New(string(source))
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		printParserErrors(os.Stdout, p.Errors())
+		os.Exit(1)
+	}
+
+	env := object.NewEnvironment()
+	evaluator.Eval(program, env)
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
