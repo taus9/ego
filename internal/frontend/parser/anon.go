5b45c80c4e61d75b09b3234ff9497ed2d535730e
Christopher Martinez <indychris239@gmail.com>
2026-01-07 18:18:34 -0500

    Refactor anon function parsing

--- internal/frontend/parser/anon.go
+++ internal/frontend/parser/anon.go
@@ -2,11 +2,14 @@ package parser
 
 import (
 	"ego/internal/frontend/ast"
+	"ego/internal/frontend/token"
 )
 
 func (p *Parser) parseAnonymousFunction() ast.Expression {
 	p.stackTrace.Push(ANON_FUNCTION)
 	anon := &ast.AnonymousFunction{Token: p.curToken}
+
+	p.nextToken() // consume ':>' token
 
 	anon.Parameters = p.parseFunctionParameters()
 
@@ -14,12 +17,15 @@ func (p *Parser) parseAnonymousFunction() ast.Expression {
 		return nil
 	}
 
+	p.blockStack.Push(token.ANON_FUNCTION)
 	anon.Body = p.parseBlockStatement()
+	p.blockStack.Pop()
 
 	if p.errorExist() {
 		return nil
 	}
 
+	//END BLOCK token already consumed in parseBlockStatement
 	p.stackTrace.Pop()
 	return anon
 }
