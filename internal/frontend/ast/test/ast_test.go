d344270a10265adaf991e62b83945ab64c6dd8e7
Christopher Martinez <indychris239@gmail.com>
2026-01-14 18:23:48 -0500

    Refactor ast test structure

--- internal/frontend/ast/test/ast_test.go
+++ internal/frontend/ast/test/ast_test.go
@@ -1,20 +1,21 @@
 package ast
 
 import (
+	"ego/internal/frontend/ast"
 	"ego/internal/frontend/token"
 	"testing"
 )
 
 func TestString(t *testing.T) {
-	program := &Program{
-		Statements: []Statement{
-			&LetStatement{
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
 				Token: token.Token{Type: token.LET_INTERNAL, Literal: "$LET"},
-				Name: &Identifier{
+				Name: &ast.Identifier{
 					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
 					Value: "myVar",
 				},
-				Value: &Identifier{
+				Value: &ast.Identifier{
 					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
 					Value: "anotherVar",
 				},
