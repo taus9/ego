4ddb7d046026978ab199334df95564db64d45d10
Christopher Martinez <indychris239@gmail.com>
2026-01-21 16:02:49 -0500

    Fix ok builtin

--- internal/backend/eval/evaluator/builtins.go
+++ internal/backend/eval/evaluator/builtins.go
@@ -44,7 +44,7 @@ var builtins = map[string]*object.Builtin{
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
 			}
 			switch args[0].Type() {
-			case object.UNHANDLED_ERROR_OBJ:
+			case object.ERROR_OBJ:
 				return FALSE
 			default:
 				return TRUE
