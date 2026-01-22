f06e8fc9fc6fd5292e43cf106b375abc0d780651
Christopher Martinez <indychris239@gmail.com>
2026-01-21 21:34:46 -0500

    Put builtins in alpha order

--- internal/backend/eval/evaluator/builtins.go
+++ internal/backend/eval/evaluator/builtins.go
@@ -8,6 +8,27 @@ import (
 
 var builtins = map[string]*object.Builtin{
 
+	"append": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("first argument must be ARRAY, got %s", args[0].Type())
+			}
+
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+
+			newElements := make([]object.Object, length+1)
+			copy(newElements, array.Elements)
+			newElements[length] = args[1]
+
+			return &object.Array{Elements: newElements}
+		},
+	},
+
 	"clear": {
 
 		// clears all elements from an array or map
@@ -81,64 +102,6 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"type": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			return &object.String{Value: string(args[0].Type())}
-		},
-	},
-
-	"put": {
-		Fn: func(args ...object.Object) object.Object {
-			for _, arg := range args {
-				fmt.Println(arg.Inspect())
-			}
-			return NIL
-		},
-	},
-
-	"len": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-
-			switch arg := args[0].(type) {
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			case *object.Map:
-				return &object.Integer{Value: int64(len(arg.Pairs))}
-			default:
-				return newError("argument to 'len' not supported, got %s", args[0].Type())
-			}
-		},
-	},
-
-	"append": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2", len(args))
-			}
-
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("first argument to 'push' must be ARRAY, got %s", args[0].Type())
-			}
-
-			array := args[0].(*object.Array)
-			length := len(array.Elements)
-
-			newElements := make([]object.Object, length+1)
-			copy(newElements, array.Elements)
-			newElements[length] = args[1]
-
-			return &object.Array{Elements: newElements}
-		},
-	},
-
 	"float": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
@@ -161,6 +124,7 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+
 	"int": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
@@ -184,12 +148,49 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
+	"len": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Map:
+				return &object.Integer{Value: int64(len(arg.Pairs))}
+			default:
+				return newError("argument to 'len' not supported, got %s", args[0].Type())
+			}
+		},
+	},
+
+	"put": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NIL
+		},
+	},
+
 	"str": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
 			}
 			return &object.String{Value: args[0].Inspect()}
+		},
+	},
+
+	"type": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			return &object.String{Value: string(args[0].Type())}
 		},
 	},
 }
